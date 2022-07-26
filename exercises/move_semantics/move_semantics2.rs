4a89bab2d39cdc3d1c260b1c4e9cde5ba4692b8a
Cem Akarsubasi <cakarsubasi18@ku.edu.tr>
2022-07-26 23:56:13 +0300

    halfway done

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -2,21 +2,33 @@
 // Make me compile without changing line 13 or moving line 10!
 // Execute `rustlings hint move_semantics2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let vec0 = Vec::new();
+    let mut vec0 : Vec<i32> = Vec::new();
 
-    let mut vec1 = fill_vec(vec0);
+    // 1 - copy outside
+    // let mut vec0_copy : Vec<i32> = Vec::new();
+    // for elem in &vec0 {
+    //     vec0_copy.push(*elem);
+    // }
+    // let mut vec1 = fill_vec(vec0_copy);
+
+    // 2 - copy inside
+    //let mut vec1 = fill_vec_ref(&vec0);
+    // copy outside
+    //let mut vec1 = vec0;
+    fill_vec_mut_ref(&mut vec0);
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
 
-    vec1.push(88);
+    //vec1.push(88);
+    vec0.push(88);
 
-    println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+    //println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+    println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
 }
 
+// 1 - copy outside
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
     let mut vec = vec;
 
@@ -25,4 +37,26 @@ fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
     vec.push(66);
 
     vec
+}
+
+// 2 - copy inside
+fn fill_vec_ref(vec: &Vec<i32>) -> Vec<i32> {
+    let mut vec2 = Vec::new();
+
+    for elem in vec {
+        vec2.push(*elem);
+    }
+
+    vec2.push(22);
+    vec2.push(44);
+    vec2.push(66);
+
+    vec2
+}
+
+fn fill_vec_mut_ref(vec: &mut Vec<i32>) -> () {
+
+    vec.push(22);
+    vec.push(44);
+    vec.push(66);
 }
--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -18,7 +18,6 @@
 // - The output element is going to be a Vector of strings.
 // Execute `rustlings hint quiz2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 pub enum Command {
     Uppercase,
@@ -29,12 +28,26 @@ pub enum Command {
 mod my_module {
     use super::Command;
 
-    // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
-        // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+    // TODO: Complete the function signature! Done!
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        // TODO: Complete the output declaration! Done!
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            let string2 = match command {
+                Command::Uppercase => string.to_uppercase(),
+                Command::Trim => string.trim().to_string(),
+                Command::Append(size) => {
+                    let mut i = 0;
+                    let mut string = string.to_string();
+                    while i < *size {
+                        string.push_str("bar");
+                        i += 1;
+                    }
+                    string
+                }
+            };
+            output.push((*string2).to_string());
         }
         output
     }
@@ -42,8 +55,8 @@ mod my_module {
 
 #[cfg(test)]
 mod tests {
-    // TODO: What to we have to import to have `transformer` in scope?
-    use ???;
+    // TODO: What to we have to import to have `transformer` in scope? Done!
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
